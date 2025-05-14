1fc073aaf1f7121ce2b5d0208da80d92549d3a36
Emily Han <emilyhyf@myth64.stanford.edu>
2025-05-14 00:52:18 -0700

    Added type checking for static dispatch

--- PS3/semant.cc
+++ PS3/semant.cc
@@ -417,7 +417,58 @@ Symbol dispatch_class::type_check(ClassTableP classtable, EnvironmentP env) {
 }
 
 Symbol static_dispatch_class::type_check(ClassTableP classtable, EnvironmentP env) {
-  return Int;
+  // type of e0
+  Symbol expr_type = expr->type_check(classtable, env);
+  Symbol dispatch_class;
+  if (expr_type == SELF_TYPE) {
+    dispatch_class = env->get_class_type();
+  } else {
+    dispatch_class = expr_type;
+  }
+
+  // Check if class exists
+  InheritanceNodeP T_node_class = classtable->lookup(type_name);
+  if (T_node_class == nullptr) {
+    classtable->semant_error() << "Cannot dispatch on undefined class" << endl;
+    this->set_type(Object);
+    return Object;
+  }
+  // Check if e0 class is a child of T
+  if (!classtable->is_ancestor(dispatch_class, type_name)) {
+    classtable->semant_error() << "Expression type is not a child of the static dispatch type" << endl;
+  }
+  // Check if method exists in class
+  method_class* method = T_node_class->get_env()->lookup_method(name);
+  if (method == nullptr) {
+    classtable->semant_error() << "Method " << name << "does not exist in class " << type_name << endl;
+    this->set_type(Object);
+    return Object;
+  }
+  // Check if parameters match
+  Formals formals = method->get_formals();
+  if (formals->len() != actual->len()) {
+    classtable->semant_error() << "Method has different number of arguments as dispatch" << endl;
+  }
+  //  the static type of the ith actual parameter must conform to the declared type of the ith formal parameter
+  for(int i = actual->first(), j = formals->first(); actual->more(i); i = actual->next(i), j = formals->next(j)) {
+    Symbol actual_type = actual->nth(i)->type_check(classtable, env);
+    Symbol formal_type = formals->nth(j)->get_formal_type();
+    if (!classtable->is_ancestor(actual_type, formal_type)) {
+      classtable->semant_error() << "Arguments do not match the expected parameter types for method " << name << endl;
+    }
+  }
+  // If f has return type B and B is a class name, then the static type of the dispatch is B. 
+  // Otherwise, if f has return type SELF_TYPE, then static type of the dispatch is A.
+  Symbol method_return_type = method->get_return_type();
+  Symbol final_type;
+
+  if (method_return_type != SELF_TYPE) {
+    final_type = method_return_type;  // B (refer to Cool manual)
+  } else {
+    final_type = expr_type;  // A (refer to Cool manual)
+  }
+  this->set_type(final_type);
+  return final_type;
 }
 
 Symbol assign_class::type_check(ClassTableP classtable, EnvironmentP env) {
